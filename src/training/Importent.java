99b88df93a5e2dbc077c586d6fe8a18f38d55231
iamvigneshwaran <vigneshwarana1511@gmail.com>
2023-01-11 23:55:36 +0530

    main file

---
+++ src/training/Importent.java
@@ -0,0 +1,18 @@
+package training;
+
+public class Importent {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Areaoftriangle ar=new Areaoftriangle();
+
+		double trianglearea=ar.area();
+		
+		System.out.println(trianglearea);
+	}
+
+}
